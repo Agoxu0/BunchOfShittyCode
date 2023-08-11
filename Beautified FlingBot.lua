844dac69d5a02899d4bfa7dfa29498a9bf0d3e6f
Agoxu0 <88200942+Agoxu0@users.noreply.github.com>
2023-08-11 13:25:06 +0200

    Update Beautified FlingBot.lua

--- Beautified FlingBot.lua
+++ Beautified FlingBot.lua
@@ -1,15 +1,21 @@
 -- Basic  variables :>>
+local UserInputService = game:GetService("UserInputService") -- We will this  to stop fling bot
 local RunService = game:GetService("RunService")
 local Players = game.Players
 local Player = Players.LocalPlayer
 local Character = Player.Character
 local OldCFrame = Character.HumanoidRootPart.CFrame
+local DidJump = false
 -- Time for coding  (⌐▨_▨)
-RunService.Heartbeat:connect(function() -- This will loop every frame.
+_G.FlingBot = RunService.Heartbeat:connect(function() -- This will loop every frame.
 	-- This code sets the Velocity of the HumanoidRootPart to 9999,9999,9999 To be able to fling people. 
 	Character.HumanoidRootPart.Velocity = Vector3.new(9999,9999,9999)
 	-- Also we need Anti Sit! So this is a simple code.
 	Character.Humanoid.Sit = false
+end)
+-- i need to put this here to know until player jumped!
+UserInputService.JumpRequest:Connect(function()
+    DidJump = true
 end)
 -- Now we need to loop every player, and then tp to them for .1 or .2 second's
 -- How do we do it? Its very simple! (I am talking to my self and writing this code at 2 am help me please)
@@ -34,8 +40,13 @@ repeat
 			end
 		end
 	end
-until Character.Humanoid.Jump == true -- By this line of code you can stop this after you jump. Usefull if you want to stop flinging
-task.wait(.1)
-Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-task.wait(.1)
-Character.HumanoidRootPart.CFrame = OldCFrame
+until DidJump -- By this line of code you can stop this after you jump. Usefull if you want to stop flinging
+_G.FlingBot:Disconnect() -- This will turn off the fling bot.
+-- Code that i explained Before.
+local Start = os.clock()
+while os.clock() - Start < .5 do
+    Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0) -- return velocity back to 0,0,0 
+    Character.HumanoidRootPart.CFrame = OldCFrame -- come back to the place where we were
+    RunService.Heartbeat:Wait()
+end
+-- Thats it! Were done.  (⌐▨_▨)
